79515da6753ad8530bae70b21850a8f9706e4f62
Justinnn <63072677+Jqstln@users.noreply.github.com>
2022-03-29 18:14:12 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+    res.render('index');
+});
+
+app.get("/invite", (req, res) => { res.redirect("https://discord.com/oauth2/authorize?client_id=786679486749999214&permissions=3207851399&scope=bot%20applications.commands")});
+app.get("/discord", (req, res) => { res.redirect("https://discord.gg/GWdnwwYMjZ")});
+
+app.listen(process.env.PORT, () => console.log(`The server is now running on port ${process.env.PORT}`));
