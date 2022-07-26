ccef644db516382bc04ff407a7e7492e8300282d
Justinnn <63072677+Jqstln@users.noreply.github.com>
2022-07-26 19:31:31 +0200

    still broken

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
