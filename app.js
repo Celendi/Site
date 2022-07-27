d5606d01749b93a341acb7e97f5ec9c1175ab490
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2022-07-27 08:05:42 +0200

    fix: tailwind

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
